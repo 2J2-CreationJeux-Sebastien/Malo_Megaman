27fb741f69fdb2c13e63b9aba75a2df663d24b75
hikup100 <152992754+hikup100@users.noreply.github.com>
2024-04-04 01:31:58 -0400

    game over

--- Assets/Script/ControlerPersonnage.cs
+++ Assets/Script/ControlerPersonnage.cs
@@ -14,54 +14,63 @@ public class ControlerPersonnage : MonoBehaviour
     public float vitesseXMax;   //vitesse horizontale Maximale désirée
     public float vitesseY;      //vitesse verticale 
     public float vitesseSaut;   //vitesse de saut désirée
+    public bool partieTerminee = false;
 
     /* Détection des touches et modification de la vitesse de déplacement;
        "a" et "d" pour avancer et reculer, "w" pour sauter
     */
     void Update ()
     {
-        // déplacement vers la gauche
-        if (Input.GetKey("a"))
+        if (partieTerminee == false) 
         {
-            vitesseX = -vitesseXMax;
-            GetComponent<SpriteRenderer>().flipX=true;
+            // déplacement vers la gauche
+            if (Input.GetKey("a"))
+            {
+                vitesseX = -vitesseXMax;
+                GetComponent<SpriteRenderer>().flipX = true;
 
-        }
-        else if (Input.GetKey("d"))   //déplacement vers la droite
-        {
-            vitesseX = vitesseXMax;
-            GetComponent<SpriteRenderer>().flipX=false;
-        }
-        else
-        {
-            vitesseX = GetComponent<Rigidbody2D>().velocity.x;  //mémorise vitesse actuelle en X
-        }
+            }
+            else if (Input.GetKey("d"))   //déplacement vers la droite
+            {
+                vitesseX = vitesseXMax;
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else
+            {
+                vitesseX = GetComponent<Rigidbody2D>().velocity.x;  //mémorise vitesse actuelle en X
+            }
 
-        print( Physics2D.OverlapCircle(transform.position, 0.5f) == true);
+            print(Physics2D.OverlapCircle(transform.position, 0.5f) == true);
 
-        // sauter l'objet à l'aide la touche "w"
-        if (Input.GetKeyDown("w") && Physics2D.OverlapCircle(transform.position, 0.5f))
-        {
-            vitesseY = vitesseSaut;
-            GetComponent<Animator>().SetBool("jump", true);
-        }
-        else
-        {
-            vitesseY = GetComponent<Rigidbody2D>().velocity.y;  //vitesse actuelle verticale
-        }
-        
-        //Applique les vitesses en X et Y
-        GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
+            // sauter l'objet à l'aide la touche "w"
+            if (Input.GetKeyDown("w") && Physics2D.OverlapCircle(transform.position, 0.5f))
+            {
+                vitesseY = vitesseSaut;
+                if (partieTerminee == false) 
+                {
+                    GetComponent<Animator>().SetBool("jump", true);
+                }      
+            }
+            else
+            {
+                vitesseY = GetComponent<Rigidbody2D>().velocity.y;  //vitesse actuelle verticale
+            }
+
+            //Applique les vitesses en X et Y
+            GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
 
 
-        //**************************Gestion des animaitons de course et de repos********************************
-        //Active l'animation de course si la vitesse de déplacement n'est pas 0, sinon le repos sera jouer par Animator
+            //**************************Gestion des animaitons de course et de repos********************************
+            //Active l'animation de course si la vitesse de déplacement n'est pas 0, sinon le repos sera jouer par Animator
 
-        if (vitesseX > 0.1f || vitesseX < -0.1f) 
-        {
-            GetComponent<Animator>().SetBool("sprint", true);
-        } else {
-            GetComponent<Animator>().SetBool("sprint", false);
+            if (vitesseX > 0.1f || vitesseX < -0.1f)
+            {
+                GetComponent<Animator>().SetBool("sprint", true);
+            }
+            else
+            {
+                GetComponent<Animator>().SetBool("sprint", false);
+            }
         }
     }
     void OnCollisionEnter2D(Collision2D infoCollision)
@@ -72,6 +81,7 @@ public class ControlerPersonnage : MonoBehaviour
         }
         if (infoCollision.gameObject.name == "Bombe")
         {
+            partieTerminee = true;
             GetComponent<Animator>().SetTrigger("death");
             Invoke("RecomencerJeu", 2f);
         }
